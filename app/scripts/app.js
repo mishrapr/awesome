6b0b0fbf6009e0a2a9a74a204475f3f53a6f8e5c
Prabhat <prabhat.mishra@ge.com>
2015-07-14 10:07:35 +0530

    TestScaffoliding

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name frontEndApp
+ * @description
+ * # frontEndApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('frontEndApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
